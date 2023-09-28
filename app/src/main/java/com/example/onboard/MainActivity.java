04fc91a90b343f54134cb91fdcd88a7513385a56
Merillax <artem-gusev-04@mail.ru>
2023-09-28 13:05:34 +0300

    vadik

---
+++ app/src/main/java/com/example/onboard/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.onboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        public void onClick(View v){
+            Intent intent = new Intent(this,onboard.class);
+            startActivity(intent);
+    }
+}
